04a40cde30ccde702bb2f97146fec4b4b7801e00
XITASO\pawel.baran <pawel.baran@xitaso.com>
2024-10-01 18:10:42 +0200

    DiscoveryService added

--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -3,15 +3,17 @@ import './App.css';
 import { AasViewer } from './AasViewer';
 import { Box, Button, TextField } from '@mui/material';
 import { AssetAdministrationShell } from '@aas-core-works/aas-core3.0-typescript/dist/types/types';
+import { DiscoveryService } from './Services/DiscoveryService';
 
 
 function App() {
+    const [assetId, setAssetId] = useState("")
     const [aas, setAas] = useState<AssetAdministrationShell>()
-    
 
-    function loadAsset() {
 
-        return undefined;
+    async function loadAsset() {
+        const discoveryService = DiscoveryService.create("https://oi4-sps24-mnestix-api.azurewebsites.net/discovery");
+        const aasId = await discoveryService.getAasIdFromDiscovery(assetId);
     }
 
     return (
@@ -19,10 +21,13 @@ function App() {
             <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mt={5}>
                 <h3>Search for your Asset</h3>
                 <Box display="flex" flexDirection="row">
-                <Box mr={2}>
-                    <TextField id="assetIdInput" label="Asset ID" variant="outlined"/>
-                </Box>
-                    <Button variant="contained">Contained</Button>
+                    <Box mr={2}>
+                        <TextField id="assetIdInput" label="Asset ID" variant="outlined"
+                                   onChange={(e) => setAssetId(e.target.value)}/>
+                    </Box>
+                    <Button onClick={() => {
+                        loadAsset()
+                    }} variant="contained">Contained</Button>
                 </Box>
                 <Box mt={5}>
                     <AasViewer></AasViewer>
---
+++ incoming-goods-app/src/Services/DiscoveryService.ts
@@ -0,0 +1,82 @@
+﻿import { Buffer } from 'buffer';
+
+export class DiscoveryService {
+    private constructor(
+        protected readonly discoveryServiceClient: DiscoveryServiceClient) {
+    }
+
+    static create(_baseUrl: string = ''): DiscoveryService {
+    const discoveryServiceClient = new DiscoveryServiceClient(_baseUrl);
+    return new DiscoveryService(discoveryServiceClient);
+    }
+    
+    async getAasIdFromDiscovery(searchAssetId: string): Promise<string[] | null> {
+        try {
+            if (!searchAssetId) {
+                throw new Error();
+            }
+            const aasIds = (await this.discoveryServiceClient.getAasIdsByAssetId(searchAssetId)).result;
+
+            if (aasIds.length === 0) {
+                throw new Error();
+            }
+
+            return aasIds;
+        } catch (e) {
+            console.warn('Could not be found in the discovery service, will continue to look in the AAS registry');
+            return null;
+        }
+    }
+}
+
+export class DiscoveryServiceClient {
+    private readonly baseUrl: string;
+
+    constructor(
+        protected _baseUrl: string = '') {
+        this.baseUrl = _baseUrl;
+    }
+
+    async getAasIdsByAssetId(assetId: string) {
+        return this.getAllAssetAdministrationShellIdsByAssetLink([
+            {
+                name: 'globalAssetId',
+                value: assetId
+            }
+        ]);
+    }
+
+    async getAllAssetAdministrationShellIdsByAssetLink(
+        assetIds: { name: string; value: string }[]
+    ): Promise<{ paging_metadata: string; result: string[] }> {
+        const headers = {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        };
+
+        const url = new URL(`${this.baseUrl}/lookup/shells`);
+
+        assetIds.forEach((obj) => {
+            url.searchParams.append('assetIds', encodeBase64(JSON.stringify(obj)));
+        });
+
+        const response = await fetch(url.toString(), {
+            method: 'GET',
+            headers
+        });
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw response;
+        }
+    }
+}
+
+export function encodeBase64(str: string): string {
+    return Base64EncodeUrl(Buffer.from(str).toString('base64'));
+}
+
+function Base64EncodeUrl(str: string) {
+    return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
