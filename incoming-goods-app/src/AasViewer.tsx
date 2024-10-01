b633c400701314b6b52c4ac1ab7c8cf58cb3e7f4
melanie.gail <melanie.gail@xitaso.com>
2024-10-01 17:35:37 +0200

    added empty aas form

--- incoming-goods-app/src/AasViewer.tsx
+++ incoming-goods-app/src/AasViewer.tsx
@@ -1,3 +1,38 @@
-export function AasViewer() {
-    return (<>Aas will be displayed here.</>)
+import { Box, Button, Card, TextField } from "@mui/material";
+import React, { useState } from 'react';
+
+export function AasViewer(props: {}) {
+    const [color, setColor] = useState("")
+    const [weight, setWeight] = useState(0)
+    const [material, setMaterial] = useState("")
+
+    function saveAas() {
+        console.log("save")
+    }
+    
+    return (
+        <Card>
+            <Box display="flex" padding={5}>
+                <Box width="100px" height="100px" border="3px solid grey" mr={5}>Image</Box>
+                <Box display="flex" flexDirection="column">
+                    <p>Supplier XYZ</p>
+                    <p>Asset 12sfavr2oifdj</p>
+                    <Box display="flex" flexDirection="column">
+                        <form onSubmit={() => {saveAas()}}>
+                            <Box mb={2}>
+                                <TextField label="Color" onChange={(e) => setColor(e.target.value)}></TextField>
+                            </Box>
+                            <Box mb={2}>
+                                <TextField label="Weight" type="number" onChange={(e) => setWeight(Number(e.target.value))}></TextField>
+                            </Box>
+                            <Box mb={2}>
+                                <TextField label="Material" onChange={(e) => setMaterial(e.target.value)}></TextField>
+                            </Box>
+                            <Button variant="contained" type="submit">Save</Button>
+                        </form>
+                    </Box>
+                </Box>
+            </Box>
+        </Card>
+    )
 }
--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { AasViewer } from './AasViewer';
 import { Box, Button, TextField } from '@mui/material';
+import { AssetAdministrationShell } from '@aas-core-works/aas-core3.0-typescript/dist/types/types';
 
 function App() {
+    const [aas, setAas] = useState<AssetAdministrationShell>()
     
     function loadAsset() {
         // TODO load from Discovery, Registry, Repos
@@ -12,13 +14,15 @@ function App() {
 
     return (
     <div className="App">
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mt={5}>
             <h3>Search for your Asset</h3>
             <Box display="flex" flexDirection="row">
-                <TextField id="assetIdInput" label="Asset ID" variant="outlined" />
+                <Box mr={2}>
+                    <TextField id="assetIdInput" label="Asset ID" variant="outlined" />
+                </Box>
                 <Button variant="contained">Contained</Button>
             </Box>
-            <Box>
+            <Box mt={5}>
                 <AasViewer></AasViewer>
             </Box>
         </Box>
