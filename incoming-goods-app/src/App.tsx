905d54a29e9b7b84364c7f61d332ed4d22ae01ad
XITASO\pawel.baran <pawel.baran@xitaso.com>
2024-10-01 16:43:05 +0200

    @aas-core-works/aas-core3.0-typescript package

--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -3,27 +3,28 @@ import './App.css';
 import { AasViewer } from './AasViewer';
 import { Box, Button, TextField } from '@mui/material';
 
+
 function App() {
-    
+
     function loadAsset() {
-        // TODO load from Discovery, Registry, Repos
+
         return undefined;
     }
 
     return (
-    <div className="App">
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <h3>Search for your Asset</h3>
-            <Box display="flex" flexDirection="row">
-                <TextField id="assetIdInput" label="Asset ID" variant="outlined" />
-                <Button variant="contained">Contained</Button>
+        <div className="App">
+            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                <h3>Search for your Asset</h3>
+                <Box display="flex" flexDirection="row">
+                    <TextField id="assetIdInput" label="Asset ID" variant="outlined"/>
+                    <Button variant="contained">Contained</Button>
+                </Box>
+                <Box>
+                    <AasViewer></AasViewer>
+                </Box>
             </Box>
-            <Box>
-                <AasViewer></AasViewer>
-            </Box>
-        </Box>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
