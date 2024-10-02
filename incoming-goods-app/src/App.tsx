a6fdd34eb18cd0414ec7124bc0b86051570990eb
Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
2024-10-02 08:30:30 +0000

    Implements functionality to fetch the Descriptor from AAS Registry

--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -4,16 +4,28 @@ import { AasViewer } from './AasViewer';
 import { Box, Button, TextField } from '@mui/material';
 import { AssetAdministrationShell } from '@aas-core-works/aas-core3.0-typescript/dist/types/types';
 import { DiscoveryService } from './Services/DiscoveryService';
+import { AasRegistry } from './Services/AasRegistry';
 
 
 function App() {
     const [assetId, setAssetId] = useState("")
     const [aas, setAas] = useState<AssetAdministrationShell>()
 
-
     async function loadAsset() {
         const discoveryService = DiscoveryService.create("https://oi4-sps24-mnestix-api.azurewebsites.net/discovery");
         const aasId = await discoveryService.getAasIdFromDiscovery(assetId);
+
+        if (!aasId || aasId.length <= 0) {
+            throw new Error("The AAS ID list from AAS Discovery is empty");
+        }
+
+        const aasRegistry = AasRegistry.create("https://mnestix-basyx-aas-registry-b36325a9.azurewebsites.net");
+        const concernedAasId = aasId[0];
+        const aasDescriptor = await aasRegistry.getAasDescriptorFromRegistry(concernedAasId);
+
+        console.log("AAS Descriptor: " + JSON.stringify(aasDescriptor));
+
+
     }
 
     return (
@@ -23,7 +35,7 @@ function App() {
                 <Box display="flex" flexDirection="row">
                     <Box mr={2}>
                         <TextField id="assetIdInput" label="Asset ID" variant="outlined"
-                                   onChange={(e) => setAssetId(e.target.value)}/>
+                            onChange={(e) => setAssetId(e.target.value)} />
                     </Box>
                     <Button onClick={() => {
                         loadAsset()
---
+++ incoming-goods-app/src/Services/AasRegistry.ts
@@ -0,0 +1,66 @@
+﻿import { Buffer } from 'buffer';
+import { AssetAdministrationShellDescriptor } from '../interfaces';
+
+export class AasRegistry {
+    private constructor(
+        protected readonly aasRegistryClient: AasRegistryClient) {
+    }
+
+    static create(_baseUrl: string = ''): AasRegistry {
+    const aasRegistryClient = new AasRegistryClient(_baseUrl);
+    return new AasRegistry(aasRegistryClient);
+    }
+    
+    async getAasDescriptorFromRegistry(aasId: string): Promise<AssetAdministrationShellDescriptor | null> {
+        try {
+            if (!aasId) {
+                throw new Error();
+            }
+
+            const aasDescriptor = (await this.aasRegistryClient.getAasDescriptorByAasId(aasId));
+
+            return aasDescriptor;
+        } catch (e) {
+            console.error('Error: Could not be found in the AAS registry');
+            return null;
+        }
+    }
+}
+
+export class AasRegistryClient {
+    private readonly baseUrl: string;
+
+    constructor(
+        protected _baseUrl: string = '') {
+        this.baseUrl = _baseUrl;
+    }
+
+    async getAasDescriptorByAasId(aasId: string) {
+        return this.getAssetAdministrationShellDescriptorsByAasId(aasId);
+    }
+
+    async getAssetAdministrationShellDescriptorsByAasId(
+        aasId: string
+    ): Promise<AssetAdministrationShellDescriptor> {
+
+        const url = new URL(`${this.baseUrl}/shell-descriptors/${encodeBase64(aasId)}`);
+
+        const response = await fetch(url.toString(), {
+            method: 'GET'
+        });
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw response;
+        }
+    }
+}
+
+export function encodeBase64(str: string): string {
+    return Base64EncodeUrl(Buffer.from(str).toString('base64'));
+}
+
+function Base64EncodeUrl(str: string) {
+    return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
