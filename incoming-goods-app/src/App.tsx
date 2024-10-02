a7ac375f0e16e1e0d91459eed79d176f10e95fd6
melanie.gail <melanie.gail@xitaso.com>
2024-10-02 09:24:58 +0200

    added theme and logo

--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
 import { AasViewer } from './AasViewer';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, createTheme, TextField, ThemeProvider } from '@mui/material';
 import { AssetAdministrationShell } from '@aas-core-works/aas-core3.0-typescript/dist/types/types';
 import { DiscoveryService } from './Services/DiscoveryService';
-
+import logo from './OI4Logo.png'
 
 function App() {
     const [assetId, setAssetId] = useState("")
@@ -16,8 +16,23 @@ function App() {
         const aasId = await discoveryService.getAasIdFromDiscovery(assetId);
     }
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#76B82A',
+            },
+            secondary: {
+                main: '#757B7F',
+            },
+        },
+    });
+
     return (
+        <ThemeProvider theme={theme}>
         <div className="App">
+            <Box sx={{ flexGrow: 1 }}>
+                <img width="300px" src={logo}/>
+            </Box>
             <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mt={5}>
                 <h3>Search for your Asset</h3>
                 <Box display="flex" flexDirection="row">
@@ -34,6 +49,7 @@ function App() {
                 </Box>
             </Box>
         </div>
+        </ThemeProvider>
     );
 }
 
