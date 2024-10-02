9ef114e86dcecd633d7a5a9cbdc11cff6c0c6648
melanie.gail <melanie.gail@xitaso.com>
2024-10-02 10:46:35 +0200

    Footer Styling

--- incoming-goods-app/src/App.tsx
+++ incoming-goods-app/src/App.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import { AasViewer } from './AasViewer';
 import { Box, Button, CircularProgress, createTheme, TextField, ThemeProvider } from '@mui/material';
-import { AssetAdministrationShell } from '@aas-core-works/aas-core3.0-typescript/dist/types/types';
 import { DiscoveryService } from './Services/DiscoveryService';
 import logo from './OI4Logo.png'
 import { Footer } from './Footer';
+import { AASAndSubmodels } from './interfaces';
 
 function App() {
     const [assetId, setAssetId] = useState("")
-    const [aas, setAas] = useState<AssetAdministrationShell>()
+    const [aas, setAas] = useState<AASAndSubmodels>()
     const [isLoading, setIsLoading] = useState(false)
 
     async function loadAsset() {
@@ -31,7 +31,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
         <div className="App">
-            <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{ flexGrow: 1 }} mt={5}>
                 <img width="300px" src={logo}/>
             </Box>
             <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mt={5}>
@@ -55,10 +55,10 @@ function App() {
                 </Box>
                 <Box mt={5}>
                     {isLoading && <CircularProgress />}
-                    {!isLoading && <AasViewer></AasViewer>}
+                    {(!isLoading && aas) && <AasViewer aasData={ aas}></AasViewer>}
                 </Box>
             </Box>
-            <Box mt={5}>
+            <Box mt={5} position="absolute" bottom="50px" width="100%">
                 <Footer/>
             </Box>
         </div>
--- incoming-goods-app/src/Footer.tsx
+++ incoming-goods-app/src/Footer.tsx
@@ -6,11 +6,11 @@ import murr from './assets/Murrelektronik.svg'
 import { Box } from '@mui/material';
 export function Footer () {
     return (
-        <Box display="flex" justifyContent="space-between" ml={10} mr={10}>
-            <img src={murr} width="250px"/>
-            <img src={metalevel} width="300px"/>
-            <img src={xitaso} width="300px"/>
-            <img src={endress} width="300px"/>
-            <img src={iese} width="300px"/>
+        <Box display="flex" justifyContent="space-between" ml={10} mr={10} gap={3}>
+            <img src={murr} width="150px"/>
+            <img src={metalevel} width="200px"/>
+            <img src={xitaso} width="200px"/>
+            <img src={endress} width="200px"/>
+            <img src={iese} width="200px"/>
         </Box>)
 }
